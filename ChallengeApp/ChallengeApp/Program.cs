21c1e03d875207872b89ed9c87381fbf759b4dd2
Sabina Trompeta <suelittlelady7@gmail.com>
2023-03-30 22:14:35 +0200

    Stworzenie aplikacji  zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Sabina";
+string sex = "kobieta";
+int age = 37;
+
+
+if (!(name == "Sabina" && age < 40))
+{
+    Console.WriteLine("KOBIETA PONIZEJ 30 LAT");
+}
+else if (age == 33)
+{
+    Console.WriteLine("EWA LAT 33");
+}
+else if (!(sex == "kobieta"));
+{
+    if (age < 20)
+    {
+
+    }
+    Console.WriteLine("NIEPELNOLETNI MEZCZYZNA");
+}
+
+
+
