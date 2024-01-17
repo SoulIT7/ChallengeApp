3122625adb0d62d087edeccdc051c3515b6abb08
SoulIT7 <suelittlelady7@gmail.com>
2024-01-17 01:08:52 +0100

    Zadanie domowe -dzien 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isWoman = true;
+var age = 25;
+
+if (isWoman == true && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (!isWoman && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
+else if (age == 30 && name == "Ewa")
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else
+{
+    Console.WriteLine("Nic");
+}
